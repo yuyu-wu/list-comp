c52d56be3011a0c2c1662e6424de89801e2d4687
yuyu <swharriet2012@gmail.com>
2024-01-19 22:20:25 +1100

    original

---
+++ list_comp.py
@@ -0,0 +1,12 @@
+# Given a list ["Elie", "Tim", "Matt"], create a variable called answer, which is a new list containing the first letter of each name in the list.  I would use a list comprehension, though you could also loop over manually.
+
+# Given a list [1,2,3,4,5,6], create a new variable called answer2, which is a new list of all the even values.  Another good candidate for a list comp.
+
+names = ['Elie', 'Tim', 'Matt']
+answer = [name[0] for name in names]
+
+nums = [1,2,3,4,5,6]
+answer2 = [num for num in nums if num % 2 == 0]
+
+print(answer)
+print(answer2)
